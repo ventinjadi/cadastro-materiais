faa777db3a1b2eb5a28550e6fb040955132352ae
ventinjadi <80498759+ventinjadi@users.noreply.github.com>
2026-09-10 15:07:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='materiais-v7';
+const CACHE='materiais-v8';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
