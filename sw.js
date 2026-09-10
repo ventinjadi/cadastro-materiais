32f33f688db55a6b8b59100f22ab6f16b4904e19
ventinjadi <80498759+ventinjadi@users.noreply.github.com>
2026-09-10 14:39:47 -0300

    Update cache version from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='materiais-v6';
+const CACHE='materiais-v7';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
