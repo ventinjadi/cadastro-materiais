7081e1c572ab1fb36be5c0a5b1f138b1ebfdff51
ventinjadi <80498759+ventinjadi@users.noreply.github.com>
2026-09-23 11:22:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='materiais-v15';
+const CACHE='materiais-v16';
 const ASSETS=['./','index.html','styles.css','custom.css','app.js','manifest.webmanifest','app-icon.png','logo-app-sumai-ufba-v2.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
